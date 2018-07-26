282bd98fb88677a23dd942743f991532c4ae159e
Sangeeta Biswas <sangeeta.biswas.bd@gmail.com>
2018-07-26 09:48:41 +0200

    Update LoadImage.py

--- LoadImage.py
+++ LoadImage.py
@@ -18,7 +18,8 @@ import numpy as np
 #	2. Declare constants
 IMG_H = 64
 IMG_W = 64
-GRAYSCALE = False	#	Color Image --> False, GrayScale Image --> True
+GRAYSCALE = False	#	Color Image --> False, 
+					#	GrayScale Image --> True
 
 #	3.	Load an image file.
 imgFile = "/home/sangeeta/retinaImage1.jpg"
@@ -42,21 +43,29 @@ print('Shape: {}, Max Val: {}, Min Val: {}'.format(imgAr.shape, np.amax(imgAr), 
 print('R: {}, G: {}, B: {}'.format(imgR.shape, imgG.shape, imgB.shape))
 
 #	6.	Display loaded image.
-fig = plt.figure(figsize=(2, 3))
+fig = plt.figure(figsize=(20, 20))
 
 subPlt1 = fig.add_subplot(2, 3, 1)
 subPlt1.set_title('RGB')
+plt.xticks([])
+plt.yticks([])
 plt.imshow(img)
 
 subPlt2 = fig.add_subplot(2, 3, 2)
 subPlt2.set_title('Red Channel')
+plt.xticks([])
+plt.yticks([])
 plt.imshow(imgR, cmap = plt.get_cmap('gray'))
 
 subPlt3 = fig.add_subplot(2, 3, 3)
 subPlt3.set_title('Green Channel')
+plt.xticks([])
+plt.yticks([])
 plt.imshow(imgG, cmap = plt.get_cmap('gray'))
 
 subPlt4 = fig.add_subplot(2, 3, 4)
 subPlt4.set_title('Blue Channel')
+plt.xticks([])
+plt.yticks([])
 plt.imshow(imgB, cmap = plt.get_cmap('gray'))
 plt.show()
